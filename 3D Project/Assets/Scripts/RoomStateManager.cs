24e4a436e1deb24cf32b5663032379347d853c6c
Sdarks <personal@sdarks.com>
2020-03-04 22:21:55 +0000

    Revert "Revert "Fix fudge factor on card. Make machine eat cards for progress""
    This reverts commit 1c0b0e344d0a750daae47fdbecb045d96f6582e5.

--- 3D Project/Assets/Scripts/RoomStateManager.cs
+++ 3D Project/Assets/Scripts/RoomStateManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RoomStateManager : MonoBehaviour
 {
+
+	public static RoomStateManager inst;
 	public SpriteRenderer GreenButton;
 	public SpriteRenderer RedButton;
 	public Sprite GreenButtonOn;
@@ -96,6 +98,20 @@ public class RoomStateManager : MonoBehaviour
 			{
 				ProgressBlobs[i].sprite = ProgressBlobOffSprites[i];
 			}
+		}
+	}
+
+
+	private void Awake()
+	{
+		if(inst == null)
+		{
+			inst = this;
+		}
+		else
+		{
+			Destroy(this);
+			Debug.LogError("Multiple RoomStateManager detected, deleting the newest");
 		}
 	}
 
