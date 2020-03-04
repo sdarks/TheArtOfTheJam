eca777e5a918cf22a5f5d035d4eeaaa124460bd2
Hamzah <hamzahhayat@live.co.uk>
2020-03-04 22:30:52 +0000

    Working on prefabs

---
+++ 3D Project/Assets/Scripts/RoomObjects/MachineHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineHandler : MonoBehaviour
+{
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Do some logic here to check if it's the correct card
+
+        RoomStateManager.inst.IncrementProgress();
+        Destroy(other.gameObject);
+    }
+}
