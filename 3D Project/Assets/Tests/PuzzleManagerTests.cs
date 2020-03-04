a1229b86fb9524991f852628c637d264ba9fd3a1
Hamzah <hamzahhayat@live.co.uk>
2020-03-04 19:47:55 +0000

    two objective test

--- 3D Project/Assets/Tests/PuzzleManagerTests.cs
+++ 3D Project/Assets/Tests/PuzzleManagerTests.cs
@@ -42,5 +42,48 @@ namespace Tests
             objectivesCompleted = manager.GetObjectivesCompleted();
             Assert.AreEqual(objectivesCompleted[0], true);
         }
+
+        // Complete an objective that requires us to first change a green card to blue, then put the blue card into another machine
+        [Test]
+        public void TurnGreenCardBlue()
+        {
+            // Use the Assert class to test conditions
+            RoomObjectBuilder objectBuilder = new RoomObjectBuilder();
+            RoomActionBuilder actionBuilder = new RoomActionBuilder();
+
+            RoomObject greenCard = objectBuilder.addProperty("type", "card").addProperty("colour", "green").build();
+            RoomObject blueCard = objectBuilder.addProperty("type", "card").addProperty("colour", "blue").build();
+
+            RoomObject colourMachine = objectBuilder.addProperty("type", "container").addProperty("name", "colourMachine").build();
+            RoomObject copyMachine = objectBuilder.addProperty("type", "container").addProperty("name", "copyMachine").build();
+
+            RoomAction placeGreenCardIntoColourMachine = actionBuilder.addObject(greenCard).addObject(colourMachine).setAction("put in").build();
+            RoomAction placeBlueCardIntoCopyMachine = actionBuilder.addObject(blueCard).addObject(copyMachine).setAction("put in").build();
+
+            List<PuzzleNode> objectives = new List<PuzzleNode>();
+
+            // Create final objective first, then create Green card objective
+            PuzzleNode copyBlueCardObjective = new PuzzleNode(null, null, placeBlueCardIntoCopyMachine);
+            PuzzleNode createBlueCardObjective = new PuzzleNode(null, copyBlueCardObjective, placeGreenCardIntoColourMachine);
+            objectives.Add(createBlueCardObjective);
+
+            PuzzleManager manager = new PuzzleManager(objectives);
+            // Setup is now complete
+
+            // Objective should start false
+            bool[] objectivesCompleted = manager.GetObjectivesCompleted();
+            Assert.AreEqual(objectivesCompleted[0], false);
+
+            // Then be completed by doing the required action
+            manager.parseAction(placeGreenCardIntoColourMachine);
+            objectivesCompleted = manager.GetObjectivesCompleted();
+            Assert.AreEqual(objectivesCompleted[0], false);
+
+            // Should still be not complete
+            // Then we put a blue card into copy machine to finish
+            manager.parseAction(placeBlueCardIntoCopyMachine);
+            objectivesCompleted = manager.GetObjectivesCompleted();
+            Assert.AreEqual(objectivesCompleted[0], true);
+        }
     }
 }
