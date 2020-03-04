d193ec00b38b2068e5f2ca09c6cd2f226bf09a4a
Sdarks <personal@sdarks.com>
2020-03-04 22:14:54 +0000

    Fix fudge factor on card. Make machine eat cards for progress

--- 3D Project/Assets/MachineHandler.cs
+++ 3D Project/Assets/MachineHandler.cs
@@ -15,4 +15,12 @@ public class MachineHandler : MonoBehaviour
     {
         
     }
+
+	private void OnTriggerEnter( Collider other )
+	{
+		//Do some logic here to check if it's the correct card
+
+		RoomStateManager.inst.IncrementProgress();
+		Destroy(other.gameObject);
+	}
 }
