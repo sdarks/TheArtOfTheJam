72962872e32ddfb8e0bf029af66e82b74bf1103d
anilbanarsee <anil.banarsee@googlemail.com>
2020-03-06 17:31:14 +0000

    Added basic graphics: Background, Table and Two Machines.
    Added basic card pick up mechanic.

--- 3D Project/Assets/Scripts/RoomObjects/Card.cs
+++ 3D Project/Assets/Scripts/RoomObjects/Card.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
 
 public class Card : MonoBehaviour
 {
     public string cardName;
     public Color colour = Color.white;
+    public bool held = false;
 
     void Init(string cardName, Color colour)
     {
@@ -14,4 +16,42 @@ public class Card : MonoBehaviour
 
         GetComponentInChildren<SpriteRenderer>().color = colour;
     }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            held = true;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.transform.name == "MyObjectName")
+                {
+                    print("My object is clicked by mouse");
+                }
+            }
+        } else if (!Input.GetMouseButton(0))
+        {
+            held = false;
+        }
+        
+        if (held)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 5;
+            Vector3 position = Camera.main.ScreenToWorldPoint(mousePosition);
+            Debug.Log(mousePosition);
+            Debug.Log(position);
+           // position.z = -5;
+           // Debug.Log(transform.rotation);
+          // transform.rotation = new Quaternion(0, -0.5f, 0.5f, 0.5f);
+            transform.position = position;
+        }
+        
+        
+    }
+
 }
