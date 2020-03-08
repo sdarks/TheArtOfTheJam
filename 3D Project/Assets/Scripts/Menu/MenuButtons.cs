16d040eab9d0391b11de3e937c83184e4076f5dd
Hamzah Hayat <hamzahhayat@live.co.uk>
2020-03-08 18:30:34 +0000

    Menu (#2)
    * start work on menu
    
    * Finish Menu, link to sample scene (for level 1 button)

---
+++ 3D Project/Assets/Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject levelSelectRoot;
+    public GameObject menuRoot;
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void LevelSelectButton()
+    {
+        levelSelectRoot.SetActive(true);
+        menuRoot.SetActive(false);
+    }
+
+    public void BackButton()
+    {
+        levelSelectRoot.SetActive(false);
+        menuRoot.SetActive(true);
+    }
+
+    public void LoadLevelButton(string levelName)
+    {
+        SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+    }
+}
