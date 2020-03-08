8e2f7bcda599284e0a2430e04f44c01b586cca75
Hamzah <hamzahhayat@live.co.uk>
2020-03-08 17:54:15 +0000

    start work on menu

---
+++ 3D Project/Assets/Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void LevelSelectButton()
+    {
+
+    }
+
+    public void BackButton()
+    {
+
+    }
+
+    public void LoadLevelButton()
+    {
+
+    }
+}
