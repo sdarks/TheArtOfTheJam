fa5e4129ea9ceb48a2c6f97ba17b953d1015f438
Sdarks <personal@sdarks.com>
2020-03-08 20:57:53 +0000

    Add reset button

--- 3D Project/Assets/Scripts/RoomStateManager.cs
+++ 3D Project/Assets/Scripts/RoomStateManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //So you can use SceneManager
 
 public class RoomStateManager : MonoBehaviour
 {
@@ -38,6 +39,11 @@ public class RoomStateManager : MonoBehaviour
 		}
 	}
 
+
+	public void ResetLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
 	// Start is called before the first frame update
 	void Start()
     {
